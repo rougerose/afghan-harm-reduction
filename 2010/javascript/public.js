dcaf86d838c659e090019e05420b3f7a2116c921
christophe le drean <christophe@rouge-rose.net>
2010-12-02 09:04:48 +0100

    suite

--- 2010/javascript/public.js
+++ 2010/javascript/public.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 	
-/*
- * Grille de mise en page ajoutée aux boutons d'administration de spip
- */	
-	$("#spip-admin").append("<a id='grille' class='spip-admin-boutons' href='#'>Grille</a>");
-	$("#grille").click(function(){
-		$("#page").toggleClass("grille");
-	});
-/*
- * Slider (cf. plugins.js)
- */
-$(".slider").slider();
+	/*
+	 * Grille de mise en page ajoutée aux boutons d'administration de spip
+	 */	
+		$("#spip-admin").append("<a id='grille' class='spip-admin-boutons' href='#'>Grille</a>");
+		$("#grille").click(function(){
+			$("#page").toggleClass("grille");
+		});
+		/*
+	 * Slider (cf. plugins.js)
+	 */
+	$(".slider").slider();
 
 
-$(".article .texte").each(function(){
-	$("p").slice(3).wrapAll('<div class="more" />').parent("div.more").hide();
-	$("p:visible:last").append(' <a href="#" class="read-more">MORE</a>');
-	$(".read-more").click(function(){
-		$(this).hide().parent().next().slideDown("slow");
-		return false;
+	$(".article .texte").each(function(){
+		$(">p",this).slice(2).wrapAll('<div class="more" />').parent("div.more").hide();
+		$("> p:visible:last",this).append(' <a href="#" class="read-more">MORE</a>');
+		$(".read-more").click(function(){
+			$(this).hide().parent().next().slideDown("slow");
+			return false;
+		});
 	});
-});
 
 
 });
