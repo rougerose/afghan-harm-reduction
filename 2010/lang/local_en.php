97d48bbddf1c35d6e9cf7837e648fa74e9368685
christophe ld <christophe@rouge-rose.net>
2011-04-13 13:38:31 +0200

    article : logo et événement

--- 2010/lang/local_en.php
+++ 2010/lang/local_en.php
@@ -6,12 +6,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'accueil' => 'Home',
 	'agenda' => 'Agenda',
 	'aucun_article_disponible' => 'Aucun article disponible',
+	'_a' => 'at',
+	'_au' => 'to',
 
 	// D
 	'dernieres_actu' => 'Last news',
+	'_du' => 'from',
 
 	// L
 	'lire_la_suite' => 'Read more',
+	'_le' => 'le [traduire]',
 
 	// P
 	'par' => 'By',
