228fc769b7bfbfee9367868aa4457d8700c45785
christophe le drean <christophe@rouge-rose.net>
2011-06-26 17:10:51 +0200

    fichier trad pashto identique au dari

--- 2010/lang/local_ps.php
+++ 2010/lang/local_ps.php
@@ -5,61 +5,155 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
-	'accueil' => 'pashto',
-	'afficher_document' => 'pashto',
-	'afficher_image' => 'pashto',
-	'agenda' => 'pashto',
-	'archives' => 'pashto',
-	'_article' => 'pashto',
-	'_articles' => 'pashto',
-	'articles' => 'Articles (traduire)',
-	'aucun_article_disponible' => 'pashto',
-	'aucun_site_disponible' => 'aucun site dispo (traduire)',
-	'_a' => 'pashto',
-	'_au' => 'pashto',
+	'about_us' => 'About us',
+	'accueil' => '',
+	'afficher_document' => '',
+	'afficher_image' => '',
+	'agenda' => '',
+	'antispam_champ_vide' => 'Please leave this field empty:',
+	'archives' => '',
+	'articles' => '',
+	'_article' => '',
+	'_articles' => '',
+	'aucun_article_disponible' => '',
+	'aucun_site_disponible' => 'No website available',
+	'_a' => '',
+	'_au' => '',
+
+	// C
+	'contact_us' => 'Contact us',
+	'credits_site' => 'Site created with SPIP. Design & development by christophe le drean',
 
 	// D
-	'dernieres_actu' => 'pashto',
-	'_document' => 'pashto',
-	'_documents' => 'pashto',
-	'document_sans_titre' => 'pashto',
-	'_du' => 'pashto',
+
+	'date_jnum1' => '1',
+	'date_jnum10' => '10',
+	'date_jnum11' => '11',
+	'date_jnum12' => '12',
+	'date_jnum13' => '13',
+	'date_jnum14' => '14',
+	'date_jnum15' => '15',
+	'date_jnum16' => '16',
+	'date_jnum17' => '17',
+	'date_jnum18' => '17',
+	'date_jnum19' => '19',
+	'date_jnum2' => '2',
+	'date_jnum20' => '20',
+	'date_jnum21' => '21',
+	'date_jnum22' => '22',
+	'date_jnum23' => '23',
+	'date_jnum24' => '24',
+	'date_jnum25' => '25',
+	'date_jnum26' => '26',
+	'date_jnum27' => '27',
+	'date_jnum28' => '28',
+	'date_jnum29' => '29',
+	'date_jnum3' => '3',
+	'date_jnum30' => '30',
+	'date_jnum31' => '31',
+	'date_jnum4' => '4',
+	'date_jnum5' => '5',
+	'date_jnum6' => '6',
+	'date_jnum7' => '7',
+	'date_jnum8' => '8',
+	'date_jnum9' => '9',
+	'date_mois_1' => 'January',
+	'date_mois_10' => 'October',
+	'date_mois_11' => 'November',
+	'date_mois_12' => 'December',
+	'date_mois_2' => 'February',
+	'date_mois_3' => 'March',
+	'date_mois_4' => 'April',
+	'date_mois_5' => 'May',
+	'date_mois_6' => 'June',
+	'date_mois_7' => 'July',
+	'date_mois_8' => 'August',
+	'date_mois_9' => 'September',
+
+	'dernieres_actu' => '',
+	'_document' => '',
+	'document_sans_titre' => '',
+	'_documents' => '',
+	'_du' => '',
+
+	// E
+	'envoi_via_le_site' => 'Sent via the site',
+	'envoyer_message' => 'to send a message',
+
+	// F
+	'facebook' => 'Facebook',
+	'follow_us' => 'Follow us',
+	'form_pet_votre_email' => 'Your e-mail address',
+	'form_prop_confirmer_envoi' => 'confirm dispatch',
+	'form_prop_indiquer_email' => 'Please enter a valid e-mail address',
+	'form_prop_envoyer' => 'Send a message',
+	'form_prop_message_envoye' => 'Message sent',
+	'form_prop_sujet' => 'Subject',
+	'forum_attention_trois_caracteres' => '<b>Warning!</b> Your title needs to be at least three characters in length.',
+	'forum_attention_dix_caracteres' => '<b>Warning!</b> Your message needs to be at least ten characters in length.',
 
 	// I
-	'image' => 'image',
-	'image_sans_titre'=> 'image',
+	'image' => '',
+	'image_sans_titre'=> '',
+
+	'info_notes' => 'Footnotes',
+	'info_obligatoire' => 'This information is required',
+	'info_passe_trop_court' =>  'Password too short.',
+	'info_rechercher' => 'Search',
+	'info_texte_message' => 'Text of your message',
 
 	// L
-	'lire_la_suite' => 'pashto',
-	'_le' => 'pashto',
+	'lire_la_suite' => '',
+
+	'_le' => 'the',
 
 	// M
-	'mots_clefs' => 'mots-clés (traduire)',
+	'mots_clefs' => 'Keywords',
 
 	// P
-	'par' => 'pashto',
-	'permalink' => 'pashto',
-	'portfolio' => 'pashto',
-	'precedent' => 'pashto',
-	'publie' => 'pashto',
+	'par' => '',
+	'pass_choix_pass' => 'Please choose your new password:',
+	'pass_erreur_acces_refuse' => '<b>Error:</b> you do not have access to this site anymore.',
+	'pass_erreur_probleme_technique' => '<b>Error:</b> this e-mail could not be sent due to a technical problem.',
+	'pass_erreur_code_inconnu' => '<b>Error:</b> this code does not match any visitor with access permission to this site.',
+	'pass_erreur_non_enregistre' => '<b>Error :</b> the address @email_oubli@ is not registered in this site.',
+	'pass_mail_passcookie' => '(this is an automated message) To retrieve your access to the site @nom_site_spip@ (@adresse_site@) Please go to the following address: @sendcookie@ You can then enter a new password and connect again to the site.',
+	'pass_nouveau_enregistre' => 'Your new password has been recorded.',
+	'pass_nouveau_pass' => 'New password',
+	'pass_ok' => 'OK',
+	'pass_oubli_mot' => 'Forgotten password',
+	'pass_rappel_login' => 'Reminder: your identifier (login) is "@login@".',
+	'pass_recevoir_mail' => 'You will receive an e-mail explaining how you can retrieve your access to the site.',
+
+	'permalink' => '',
+	'portfolio' => '',
+	'precedent' => '',
+
+	'previsualisation' => 'preview',
+
+	'publie' => '',
 
 	// R
-	'resultats_recherche_pour' => 'pashto',
-	'_rubrique' => 'pashto',
-	'_rubriques' => 'pashto',
-	'rubriques' => 'Rubriques (traduire)',
+	'recherche' => 'Search',
+	'rss' => 'RSS',
+
+	'resultats_recherche_pour' => '',
+	'_rubrique' => '',
+	'_rubriques' => '',
+	'rubriques' => '',
 
 	// S
-	'_site_web' => 'pashto',
-	'_sites_web' => 'pashto',
-	'sites_web' => 'sites web (traduire)',
-	'suivant' => 'pashto',
+	'_site_web' => '',
+	'_sites_web' => '',
+	'sites_web' => '',
+	'suivant' => '',
 
 	// T
-	'tous_articles_in_rubrique' => 'pashto',
-	'trad_article_traduction' => 'xxx',
+	'tous_articles_in_rubrique' => '',
+
+	'trad_article_traduction' => 'All the versions of this article:',
 
 	// V
-	'votre_recherche' => 'pashto'
+	'votre_recherche' => ''
 );
 ?>
