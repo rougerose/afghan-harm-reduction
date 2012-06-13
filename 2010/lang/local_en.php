70435ee698067f371fb9ed0a26dfaa98346a47f4
christophe le drean <christophe@rouge-rose.net>
2012-06-13 16:32:34 +0200

    corrections fichiers langues: - alignements - comparaisons entre les fichiers

--- 2010/lang/local_en.php
+++ 2010/lang/local_en.php
@@ -3,65 +3,67 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
-	'about_us' => 'About us',
-	'accueil' => 'Home',
-	'afficher_document' => 'Show the file',
-	'afficher_image' => 'Show the picture',
-	'agenda' => 'Agenda',
-	'archives' => 'Archives',
-	'_article' => 'article',
-	'_articles' => 'articles',
+	'about_us'                 => 'About us',
+	'accueil'                  => 'Home',
+	'afficher_document'        => 'Show the file',
+	'afficher_image'           => 'Show the picture',
+	'agenda'                   => 'Agenda',
+	'archives'                 => 'Archives',
+	'_article'                 => 'article',
+	'_articles'                => 'articles',
+   'articles'                 => 'Articles',
 	'aucun_article_disponible' => 'No articles available',
-	'_a' => 'at',
-	'_au' => 'to',
-	'aucun_site_disponible' => 'No website available',
+	'_a'                       => 'at',
+	'_au'                      => 'to',
+	'aucun_site_disponible'    => 'No website available',
 
 	// C
-	'contact_us' => 'Contact us',
-	'credits_site' => 'Site created with SPIP. Design & development by christophe le drean',
+	'contact_us'   => 'Contact us',
+	'credits_site' => 'Site created with <a href="http://www.spip.net">SPIP</a>. Design & development by <a href="http://www.rouge-rose.net">christophe le drean</a>.',
 
 	// D
-	'dernieres_actu' => 'Latest news',
-	'_document' => 'file',
+	'dernieres_actu'      => 'Latest news',
+	'_document'           => 'file',
 	'document_sans_titre' => 'file',
-	'_documents' => 'files',
-	'_du' => 'from',
+	'_documents'          => 'files',
+	'_du'                 => 'from',
 
 	// F
-	'facebook' => 'Facebook',
+	'facebook'  => 'Facebook',
 	'follow_us' => 'Follow us',
 
 	// I
-	'image' => 'picture',
-	'image_sans_titre'=> 'picture',
+	'image'            => 'picture',
+	'image_sans_titre' => 'picture',
 
 	// L
 	'lire_la_suite' => 'Read more',
-	'_le' => 'the',
+	'_le'           => 'the',
 
 	// P
-	'par' => 'By',
+	'par'       => 'By',
 	'permalink' => 'Permalink',
 	'portfolio' => 'Portfolio',
 	'precedent' => 'Previous',
-	'publie' => 'Published',
+	'publie'    => 'Published',
 
 	// R
-	'recherche' => 'Search',
-	'rss' => 'RSS',
+	'recherche'                => 'Search',
+	'rss'                      => 'RSS',
 	'resultats_recherche_pour' => 'Search results for',
-	'_rubrique' => 'section',
-	'_rubriques' => 'sections',
+	'_rubrique'                => 'section',
+	'_rubriques'               => 'sections',
+   'rubriques'                => 'Sections',
 
 	// S
-	'_site_web' => 'web site',
+	'_site_web'  => 'web site',
 	'_sites_web' => 'web sites',
-	'suivant' => 'Next',
+	'suivant'    => 'Next',
 
-	// T
-	'toutes_categories' => 'All categories',
+   // T
+	'toutes_categories'         => 'All categories',
 	'tous_articles_in_rubrique' => 'All articles of this section',
-	'trier_par_categorie' => 'Sort by categorie',
+	'trier_par_categorie'       => 'Sort by categorie',
 
 	// V
 	'votre_recherche' => 'Your search'
